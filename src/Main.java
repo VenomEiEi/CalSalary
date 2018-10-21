79a527b46c086baf5ac6da91dd052cf0fb04a4c5
sidaratkhaobang <slotsnow0301@gmail.com>
2018-10-21 15:54:59 +0700

    add projectC change requirement customer

--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,7 @@ public class Main {
 			salary = sc.nextDouble();
 			System.out.print("กรุณกรอกยอดขาย : ");
 			sale=sc.nextDouble();
-			ProjectB send= new ProjectB(id,name,salary,sale);
+			ProjectC send= new ProjectC(id,name,salary,sale);
 			send.output();
 
 }
